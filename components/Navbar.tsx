913d789f63133673405bb2dc88fc21120584ea6a
unknown <rahulharihar71@gmail.com>
2025-05-26 21:10:35 +0530

    added clerk auth functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 import Image from "next/image";
 import NavItems from "./NavItems";
@@ -12,7 +13,14 @@ const Navbar = () => {
 			</Link>
 			<div className='flex items-center gap-8'>
 				<NavItems />
-				<p>Sign In</p>
+				<SignedOut>
+					<SignInButton>
+						<button className='btn-signin'>Sign In</button>
+					</SignInButton>
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 			</div>
 		</nav>
 	);
