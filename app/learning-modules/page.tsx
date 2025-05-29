22efb4a07307a67a2286a521b8e4e709c5bc830d
unknown <rahulharihar71@gmail.com>
2025-05-29 17:49:58 +0530

    developed uui for fetching user created modules

--- app/learning-modules/page.tsx
+++ app/learning-modules/page.tsx
@@ -1,5 +1,33 @@
-const page = () => {
-	return <div>Companion Library</div>;
+import ModuleCard from "@/components/ModuleCard";
+import { getAllModules } from "@/lib/actions/module.actions";
+import { getSubjectColor } from "@/lib/utils";
+
+const page = async ({ searchParams }: SearchParams) => {
+	const filters = await searchParams;
+	const subject = filters.subject ? filters.subject : "";
+	const topic = filters.topic ? filters.topic : "";
+
+	const modules = await getAllModules({ subject, topic });
+
+	console.log(modules);
+
+	return (
+		<main>
+			<section className='flex justify-between gap-4 max-sm:flex-col'>
+				<h1>Module Library</h1>
+				<div className='flex gap-4'>Filters</div>
+			</section>
+			<section className='companions-grid'>
+				{modules.map((module) => (
+					<ModuleCard
+						key={module.id}
+						{...module}
+						color={getSubjectColor(module.subject)}
+					/>
+				))}
+			</section>
+		</main>
+	);
 };
 
 export default page;
