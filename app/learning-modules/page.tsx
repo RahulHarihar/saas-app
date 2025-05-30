1d85833dacb2265722e747f7f4c2902b7548d88f
unknown <rahulharihar71@gmail.com>
2025-05-30 16:24:13 +0530

    added dearch and filter components in My Learning section

--- app/learning-modules/page.tsx
+++ app/learning-modules/page.tsx
@@ -1,4 +1,6 @@
 import ModuleCard from "@/components/ModuleCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 import { getAllModules } from "@/lib/actions/module.actions";
 import { getSubjectColor } from "@/lib/utils";
 
@@ -15,7 +17,10 @@ const page = async ({ searchParams }: SearchParams) => {
 		<main>
 			<section className='flex justify-between gap-4 max-sm:flex-col'>
 				<h1>Module Library</h1>
-				<div className='flex gap-4'>Filters</div>
+				<div className='flex gap-4'>
+					<SearchInput />
+					<SubjectFilter />
+				</div>
 			</section>
 			<section className='companions-grid'>
 				{modules.map((module) => (
