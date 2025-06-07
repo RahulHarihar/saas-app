65cb659bb1d75cc651eadd70af0caa0002154bdd
unknown <rahulharihar71@gmail.com>
2025-06-07 23:19:53 +0530

    integrated vapi steup and logic

--- components/ModuleComponent.tsx
+++ components/ModuleComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn, getSubjectColor } from "@/lib/utils";
+import { cn, configureAssistant, getSubjectColor } from "@/lib/utils";
 import { vapi } from "@/lib/vapi.sdk";
 import Image from "next/image";
 import React, { useEffect, useState, useRef } from "react";
@@ -26,6 +26,7 @@ const ModuleComponent = ({
 }: ModuleComponentProps) => {
 	const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
 	const [isSpeaking, setIsSpeaking] = useState(false);
+	const [isMuted, setIsMuted] = useState(false);
 
 	const lottieRef = useRef<LottieRefCurrentProps>(null);
 	useEffect(() => {
@@ -67,6 +68,34 @@ const ModuleComponent = ({
 			vapi.off("speech-end", onSpeechEnd);
 		};
 	}, []);
+
+	const toggleMicrophone = () => {
+		const isMuted = vapi.isMuted();
+		vapi.setMuted(!isMuted);
+		setIsMuted(!isMuted);
+	};
+
+	const handleCall = async () => {
+		setCallStatus(CallStatus.CONNECTING);
+
+		const assistantOverrides = {
+			variableValues: {
+				subject,
+				topic,
+				style,
+			},
+			clientMessages: ["transcript"],
+			serverMessages: [],
+		};
+
+		//@ts-expect-error
+		vapi.start(configureAssistant(voice, style), assistantOverrides);
+	};
+
+	const handleDisconnect = () => {
+		setCallStatus(CallStatus.FINISHED);
+		vapi.stop();
+	};
 	return (
 		<section className='flex flex-col h-[70vh]'>
 			<section className='flex gap-8 max-sm:flex-col'>
@@ -119,8 +148,41 @@ const ModuleComponent = ({
 						/>
 						<p className='font-bold text-2xl'>{userName}</p>
 					</div>
-					
+					<button
+						className='btn-mic'
+						onClick={toggleMicrophone}
+						disabled={callStatus !== CallStatus.ACTIVE}>
+						<Image
+							src={isMuted ? "/icons/mic-off.svg" : "/icons/mic-on.svg"}
+							alt='mic'
+							width={36}
+							height={36}
+						/>
+						<p className='max-sm:hidden'>
+							{isMuted ? "Turn on microphone" : "Turn off microphone"}
+						</p>
+					</button>
+					<button
+						className={cn(
+							"rounded-lg py-2 cursor-pointer transition-colors w-full text-white",
+							callStatus === CallStatus.ACTIVE ? "bg-red-700" : "bg-black",
+							callStatus === CallStatus.CONNECTING && "animate-pulse"
+						)}
+						onClick={
+							callStatus === CallStatus.ACTIVE ? handleDisconnect : handleCall
+						}>
+						{callStatus === CallStatus.ACTIVE
+							? "End Session"
+							: callStatus === CallStatus.CONNECTING
+							? "Connecting"
+							: "Start Session"}
+					</button>
 				</div>
+			</section>
+
+			<section className='transcipt'>
+				<div className='transcript-message no-scrollbar'>Messages</div>
+				<div className='transcript-fade' />
 			</section>
 		</section>
 	);
