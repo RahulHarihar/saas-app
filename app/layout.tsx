c4935c988b67b68ea292083e4670126cf7c7430c
Rahul Harihar <rahulharihar71@gmail.com>
2025-09-08 15:00:41 +0545

    fix(provider): fix Clerk provider instance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-// import { Variable } from "lucide-react";
+import Navbar from "../components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
 	variable: "--font-bricolage",
@@ -18,17 +16,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang='en'>
-			<body className={`${bricolage.variable} antialiased`}>
-				<ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+		<ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+			<html lang='en'>
+				<body className={`${bricolage.variable} antialiased`}>
 					<Navbar />
 					{children}
-				</ClerkProvider>
-			</body>
-		</html>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
